7794620369f37ff0ba22eb2aad449ca7d89b0f0c
leevn99 <leecombank@gmail.com>
2024-01-08 00:19:49 +0700

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -267,7 +267,7 @@ export default {
           await  this.getVideo()
           await  this.save_count()
         } catch (error) {
-           // window.location.href = URL_NET
+            window.location.href = URL_NET
   
         }
     },
